5dc70eaec5a3dedceaa2c06b76ff24cd3554929f
adamaddison <aaddison524@gmail.com>
2025-12-31 18:48:01 +0000

    Added code for retrieving URLs in PageService. Completed the desktop menu bar partial. Started the about view.

--- adamaddison/Controllers/PageController.cs
+++ adamaddison/Controllers/PageController.cs
@@ -14,6 +14,7 @@ namespace adamaddison.Controllers
         }
 
         [HttpGet]
+        [Route("")]
         [Route("Home")]
         public async Task<IActionResult> Home()
         {
--- adamaddison/Services/PageService.cs
+++ adamaddison/Services/PageService.cs
@@ -11,51 +11,63 @@ public class PageService : IPageService
     private readonly IHttpContentService<WorkAndEducationViewModel> _getWorkAndEducationViewModel;
     private readonly IHttpContentService<ExperienceViewModel> _getExperienceViewModel;
     private readonly IHttpContentService<AboutViewModel> _getAboutViewModel;
+    private readonly string HomeContentUrl;
+    private readonly string PortfolioContentUrl;
+    private readonly string WorkAndEducationContentUrl;
+    private readonly string ExperienceContentUrl;
+    private readonly string AboutContentUrl;
 
     public PageService(IHttpContentService<HomeViewModel> getHomeViewModel,
                        IHttpContentService<PortfolioViewModel> getPortfolioViewModel,
                        IHttpContentService<WorkAndEducationViewModel> getWorkAndEducationViewModel,
                        IHttpContentService<ExperienceViewModel> getExperienceViewModel,
-                       IHttpContentService<AboutViewModel> getAboutViewModel)
+                       IHttpContentService<AboutViewModel> getAboutViewModel,
+                       IConfiguration config)
     {
         _getHomeViewModel = getHomeViewModel;
         _getPortfolioViewModel = getPortfolioViewModel;
         _getWorkAndEducationViewModel = getWorkAndEducationViewModel;
         _getExperienceViewModel = getExperienceViewModel;
         _getAboutViewModel = getAboutViewModel;
+
+        HomeContentUrl = config["aaHomeUrl"] ?? string.Empty;
+        PortfolioContentUrl = config["aaPortfolioUrl"] ?? string.Empty;
+        WorkAndEducationContentUrl = config["aaWorkAndEducationUrl"] ?? string.Empty;
+        ExperienceContentUrl = config["aaExperienceUrl"] ?? string.Empty;
+        AboutContentUrl = config["adamaddisonTestBlobUrl"] ?? string.Empty;
     }
 
     public async Task<AboutViewModel> GetAboutContentAsync()
     {
-        AboutViewModel vm = await _getAboutViewModel.GetContentFromUrlAsync("");
+        AboutViewModel vm = await _getAboutViewModel.GetContentFromUrlAsync(AboutContentUrl);
 
         return vm;
     }
 
     public async Task<ExperienceViewModel> GetExperienceContentAsync()
     {
-        ExperienceViewModel vm = await _getExperienceViewModel.GetContentFromUrlAsync("");
+        ExperienceViewModel vm = await _getExperienceViewModel.GetContentFromUrlAsync(ExperienceContentUrl);
 
         return vm;
     }
 
     public async Task<HomeViewModel> GetHomeContentAsync()
     {
-        HomeViewModel vm = await _getHomeViewModel.GetContentFromUrlAsync("");
+        HomeViewModel vm = await _getHomeViewModel.GetContentFromUrlAsync(HomeContentUrl);
 
         return vm;
     }
 
     public async Task<PortfolioViewModel> GetPortfolioContentAsync()
     {
-        PortfolioViewModel vm = await _getPortfolioViewModel.GetContentFromUrlAsync("");
+        PortfolioViewModel vm = await _getPortfolioViewModel.GetContentFromUrlAsync(PortfolioContentUrl);
 
         return vm;
     }
 
     public async Task<WorkAndEducationViewModel> GetWorkAndEducationContentAsync()
     {
-        WorkAndEducationViewModel vm = await _getWorkAndEducationViewModel.GetContentFromUrlAsync("");
+        WorkAndEducationViewModel vm = await _getWorkAndEducationViewModel.GetContentFromUrlAsync(WorkAndEducationContentUrl);
 
         return vm;
     }
